41ab38a08c218af5ac85cc20fc8e16e1da7abbeb
tawansongsang <tawansongsang.k@gmail.com>
2023-09-30 19:31:10 +0700

    adding csharp php rust and vue config

---
+++ language/vue/lsp.lua
@@ -0,0 +1,32 @@
+return {
+    'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+    --lazy = true,
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+    },
+    config = function()
+        local lsp_zero = require('lsp-zero')
+
+        lsp_zero.on_attach(function(client, bufnr)
+          -- see :help lsp-zero-keybindings
+          -- to learn the available actions
+          lsp_zero.default_keymaps({buffer = bufnr})
+        end)
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+          ensure_installed = {
+            "volar",
+            "tsserver",
+          },
+          handlers = {
+            lsp_zero.default_setup,
+          },
+        })
+    end,
+}
