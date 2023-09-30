329e7a13df7f76c4dc4216f154f7c110d751ffa9
tawansongsang <tawansongsang.k@gmail.com>
2023-09-30 10:26:50 +0700

    init nvim

---
+++ lua/tawan/set.lua
@@ -0,0 +1,21 @@
+require("tawan.indent")
+  
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+--vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 200
+
+vim.opt.colorcolumn = "80"
