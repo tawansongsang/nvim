918465323fe5d4682771bfc9aa4e74ed72daf69e
tawansongsang <tawansongsang.k@gmail.com>
2023-10-06 21:25:40 +0700

    adding paste clipboard from system

--- lua/tawan/remap.lua
+++ lua/tawan/remap.lua
@@ -14,6 +14,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>pp", [["+p]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
