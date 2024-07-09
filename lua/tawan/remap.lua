22a13b267b091f8884f8bf94750619bb671c9062
Tawansongsang Karnkawinpong <tawansongsang.k@ipthailand.go.th>
2024-07-09 14:44:02 +0700

    fix jk to esc

--- lua/tawan/remap.lua
+++ lua/tawan/remap.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "jk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
